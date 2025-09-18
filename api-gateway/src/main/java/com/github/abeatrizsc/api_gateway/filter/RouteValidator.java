516768efc0e0fc45877ebab1fac7cb65e9423879
Ana Beatriz Santucci Carmoni <anabeatrizscarmoni@gmail.com>
2025-09-18 19:13:56 -0300

    fix (gateway): fix public access permissions to getAll and getById event endpoints

--- api-gateway/src/main/java/com/github/abeatrizsc/api_gateway/filter/RouteValidator.java
+++ api-gateway/src/main/java/com/github/abeatrizsc/api_gateway/filter/RouteValidator.java
@@ -1,10 +1,12 @@
 package com.github.abeatrizsc.api_gateway.filter;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
 
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 
 @Component
@@ -12,15 +14,21 @@ public class RouteValidator {
 
     private static final AntPathMatcher pathMatcher = new AntPathMatcher();
 
-    public static final List<String> openApiEndpoints = List.of(
-            "/api/v1/auth/register",
-            "/api/v1/auth/login",
-            "/eureka"
+    public static final Map<String, List<HttpMethod>> openApiEndpoints = Map.of(
+            "/api/v1/auth/register", List.of(HttpMethod.POST),
+            "/api/v1/auth/login", List.of(HttpMethod.POST),
+            "/eureka", List.of(HttpMethod.GET),
+            "/api/v1/events", List.of(HttpMethod.GET),
+            "/api/v1/events/*", List.of(HttpMethod.GET)
     );
 
     public Predicate<ServerHttpRequest> isSecured =
-            request -> openApiEndpoints
-                    .stream()
-                    .noneMatch(uri -> pathMatcher.match(uri, request.getURI().getPath()));
+            request -> openApiEndpoints.entrySet().stream().noneMatch(entry -> {
+                String uriPattern = entry.getKey(); //open route
+                List<HttpMethod> allowedMethods = entry.getValue(); //open route allowed methods
 
+                //if the request URI matches with the open route and allowed methods contain the request method
+                return pathMatcher.match(uriPattern, request.getURI().getPath())
+                        && allowedMethods.contains(request.getMethod());
+            });
 }
--- event-service/src/main/java/com/github/abeatrizsc/event_service/exceptions/UnauthorizedEventCreationException.java
+++
@@ -1,7 +0,0 @@
-package com.github.abeatrizsc.event_service.exceptions;
-
-public class UnauthorizedEventCreationException extends RuntimeException {
-    public UnauthorizedEventCreationException(String s) {
-        super(s);
-    }
-}
